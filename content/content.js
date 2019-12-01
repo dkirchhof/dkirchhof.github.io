59dd7ed53327af07ae23eced333cef6999f4745a
dkirchhof <dkirchhof@gmail.com>
2019-12-02 00:05:16 +0100

    init

---
+++ content/content.js
@@ -0,0 +1,100 @@
+window.content = {
+    projects: [
+        {
+            title: "5code",
+            images: [ "content/5code_1.png" ],
+            description: "5code ist eine IDE für Java und Kotlin, die den Zugang zur Programmierung für Anfänger erleichtert.",
+            tags: [ "Electron", "Polymer", "JavaScript", "Java", "Kotlin" ],
+        },
+        {
+            title: "PrintMe",
+            images: [ "content/PrintMe_1.gif" ],
+            description: "Mit PrintMe lassen sich Bücher mittels HTML und CSS schreiben.",
+            tags: [ "NW.js", "Polymer", "JavaScript", "HTMLBook", "Prince" ],
+        },
+        {
+            title: "KineticDisplays",
+            images: [ "content/KineticDisplays_1.jpg", "content/KineticDisplays_2.png", "content/KineticDisplays_3.png", "content/KineticDisplays_4.png" ],
+            description: "KineticDisplays ist ein Autorentool für Rauminstallationen mit beweglichen Bildschirmen. Ein Im- und Exportmodul für AfterEffects hilft bei der Erstellung der Bildschirminhalte.",
+            tags: [ "WPF", "Unity", "C#", "Oculus Rift" ],
+        },
+        {
+            title: "Point of View",
+            images: [ "content/pov_1.png" ],
+            description: "In Point of View können Powerpoint-Dateien mit Kollegen geteilt und kommentiert werden. Das erlaubt ein schnelles und präzises Feedback für Ideen und Konzepte.",
+            tags: [ "Node.js", "React", "TypeScript", "C#", "SQL" ],
+        },
+        {
+            title: "airteacher",
+            images: [ "content/airteacher_1.png" ],
+            description: "airteacher ist eine Lernplattform für Online- und Präsenzkurse.",
+            tags: [ "Node.js", "Angular", "TypeScript", "GraphQL", "SQL" ],
+        },
+        {
+            title: "Outlive",
+            images: [ "content/outlive_1.jpg", "content/outlive_2.jpg", "content/outlive_3.jpg" ],
+            description: "Outlive ist ein Brettspiel mit Augmented-Reality-Unterstützung. Der aktuelle Spieler erhält mithilfe seines Smartphones zusätzliche Infos, die seine Mitspieler nicht erfahren dürfen.",
+            tags: [ "Unity", "AR", "Vuforia", "C#" ],
+        },
+        {
+            title: "Virtual Classroom",
+            images: [ "content/vc_1.png" ],
+            description: "Im Virtual Classroom können Webinare in der virtuellen Welt gehalten werden. Jeder Teilnehmer kann sich einen individuellen Avatar erstellen. Auf einer Leinwand können Dokumente, Bilder und Videos geteilt werden. Ebenfalls lassen sich dort gemeinsam Skizzen erstellen.",
+            tags: [ "Unity", "VR", "C#" ],
+        },
+        {
+            title: "Morgen ist jetzt",
+            images: [ "content/MorgenIstJetzt_1.jpg", "content/MorgenIstJetzt_2.jpg", "content/MorgenIstJetzt_3.jpg" ],
+            description: "Es handelt sich um drei kleine Minispiele zum Thema Energiesparen. Jedes der Spiele wurde auf eine flexible Membran projiziert. Durch das Eindrücken eben dieser können die Spiele gesteuert werden.",
+            tags: [ "Unity", "Kinect", "C#" ],
+        },
+        {
+            title: "Public Brewing",
+            images: [ "content/public_brewing_1.jpg", "content/public_brewing_2.jpg", "content/public_brewing_3.jpg" ],
+            description: "Public Brewing ist ein Projekt bestehend aus einer interaktiven Medienfassade und mehreren AR-Anwendungen, die den Brauprozess von Altbier auf eine spielerische Art erklären.",
+            tags: [ "Unity", "Metaio", "C#", "Arduino" ],
+        },
+        {
+            title: "MirrorTainment",
+            images: [ "content/MirrorTainment_1.jpg", "content/MirrorTainment_2.jpg" ],
+            description: "MirrorTainment ist ein smarter Spiegel, der das aktuelle Wetter, Veranstaltungen, Nachrichten und vieles mehr anzeigen kann. Der Spiegel lässt sich über Gesten und Sprache steuern.",
+            tags: [ "Unity", "C#", "Leap Motion" ],
+        },
+        {
+            title: "SuperJump",
+            images: [ "content/SuperJump_1.jpg", "content/SuperJump_2.jpg", "content/SuperJump_3.jpg" ],
+            description: "SuperJump ist ein Trampolin als Eingabegerät für diverse Spiele. Der Spieler steht dabei auf einem Trampolin und sieht die Welt durch eine VR-Brille. Durch Sprünge und bestimmte Gesten kann er mit der Umgebung interagieren.",
+            tags: [ "Unity", "C#", "VR", "OptiTrack", "GearVR" ],
+        },
+        {
+            title: "ZeroGravity",
+            images: [ "content/ZeroGravity_1.jpg", "content/ZeroGravity_2.jpg" ],
+            description: "In einem Drachenflieger hängend kann der Spieler durch eine virtuelle Welt fliegen. Durch seine Körperbewegungen steuert er den virtuellen Drachen. Mittels Ventilatoren, die auf die Bewegungen reagieren, wird ein immersives Erlebnis geschaffen.",
+            tags: [ "Unity", "C#", "VR", "PlayStation Move", "Kinect" ],
+        },
+        {
+            title: "DPlayer",
+            images: [ "content/dplayer_1.png" ],
+            description: "DPlayer ist ein Musikplayer zum Abspielen von lokalen Dateien und Streams.",
+            tags: [ "WPF", "C#" ],
+        },
+        {
+            title: "SilverPhones",
+            images: [ "content/SilverPhones_1.jpg" ],
+            description: "Bei den SilverPhones handelt es sich um mobile Kopfhörerverstärker für Hörgeschädigte. Es gibt für verschiedene Einschränkungen / Erkrankungen bestimmte Modi.",
+            tags: [ "PureData", "C", "Raspberry Pi" ],
+        },
+        {
+            title: "360°-Zoo",
+            images: [ "content/360_1.jpg", "content/360_2.jpg" ],
+            description: "In der Anwendung können Nutzer den Duisburger Zoo erkundschaften. Die verschiedenen Gehege liegen als 360°-Videos vor. Zu den einzelnen Tieren können zusätzliche Informationstafeln eingeblendet werden.",
+            tags: [ "Unity", "VR", "C#" ],
+        },
+        {
+            title: "Diverses",
+            images: [ "content/divers_1.png", "content/divers_2.png", "content/divers_3.png", "content/divers_4.png", "content/divers_5.jpg" ],
+            description: "",
+            tags: [ "" ],
+        }
+    ]
+};
